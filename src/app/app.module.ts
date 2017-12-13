0a052fb1326e650d61f592b8f3821c0918dcb465
paulotroguilho <paulo.troguilho@outlook.com.br>
2017-12-13 18:05:52 -0200

    Components
    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AlertModule } from 'ngx-bootstrap';
+import {BrowserAnimationsModule, NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core'  ;
 
 @NgModule({
   declarations: [
@@ -10,10 +13,19 @@ import { AlertModule } from 'ngx-bootstrap';
   ],
   imports: [
     BrowserModule,
-    [AlertModule.forRoot(),]
+    [AlertModule.forRoot(),],
+    [BrowserAnimationsModule],
+    [NoopAnimationsModule],
+    [MatButtonModule, MatCheckboxModule],
 
   ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+],
+  exports: [MatButtonModule, MatCheckboxModule],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export class PizzaPartyAppModule { }
+export class MyOwnCustomMaterialModule { }
