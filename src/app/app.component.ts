75e8c9ef2cabb0faa7758952c3480b448e541b5a
paulotroguilho <paulo.troguilho@outlook.com.br>
2017-12-14 09:44:13 -0200

    rodando bootstrap com components material
    rodando bootstrap com components material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
+
 import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {FormsModule} from '@angular/forms';
+import {CheckboxConfigurableExample} from './checkbox-configurable-example';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-//import { AppModule } from './app/app.module';
+import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 import './polyfills';
@@ -10,102 +10,14 @@ import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {
-  MatAutocompleteModule,
-  MatButtonModule,
-  MatButtonToggleModule,
-  MatCardModule,
-  MatCheckboxModule,
-  MatChipsModule,
-  MatDatepickerModule,
-  MatDialogModule,
-  MatExpansionModule,
-  MatGridListModule,
-  MatIconModule,
-  MatInputModule,
-  MatListModule,
-  MatMenuModule,
-  MatNativeDateModule,
-  MatPaginatorModule,
-  MatProgressBarModule,
-  MatProgressSpinnerModule,
-  MatRadioModule,
-  MatRippleModule,
-  MatSelectModule,
-  MatSidenavModule,
-  MatSliderModule,
-  MatSlideToggleModule,
-  MatSnackBarModule,
-  MatSortModule,
-  MatTableModule,
-  MatTabsModule,
-  MatToolbarModule,
-  MatTooltipModule,
-  MatStepperModule,
-} from '@angular/material';
-import {CheckboxConfigurableExample} from './app/checkbox-configurable-example';
+
 import {HttpModule} from '@angular/http';
-import {CdkTableModule} from '@angular/cdk/table';
 
 
-@NgModule({
-  exports: [
-    CdkTableModule,
-    MatAutocompleteModule,
-    MatButtonModule,
-    MatButtonToggleModule,
-    MatCardModule,
-    MatCheckboxModule,
-    MatChipsModule,
-    MatStepperModule,
-    MatDatepickerModule,
-    MatDialogModule,
-    MatExpansionModule,
-    MatGridListModule,
-    MatIconModule,
-    MatInputModule,
-    MatListModule,
-    MatMenuModule,
-    MatNativeDateModule,
-    MatPaginatorModule,
-    MatProgressBarModule,
-    MatProgressSpinnerModule,
-    MatRadioModule,
-    MatRippleModule,
-    MatSelectModule,
-    MatSidenavModule,
-    MatSliderModule,
-    MatSlideToggleModule,
-    MatSnackBarModule,
-    MatSortModule,
-    MatTableModule,
-    MatTabsModule,
-    MatToolbarModule,
-    MatTooltipModule,
-  ]
-})
-export class DemoMaterialModule {}
 
-@NgModule({
 
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    HttpModule,
-    DemoMaterialModule,
-    MatNativeDateModule,
-    ReactiveFormsModule,
-  ],
-
-  declarations: [CheckboxConfigurableExample],
-  bootstrap: [CheckboxConfigurableExample],
-  providers: []
-})
-export class AppModule {}
-
-platformBrowserDynamic().bootstrapModule(AppModule);
 if (environment.production) {
   enableProdMode();
 }
+platformBrowserDynamic().bootstrapModule(AppModule);
 
