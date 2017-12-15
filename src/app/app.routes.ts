23f1b9c51e24fc206d325ab304dcaf8ad6b77b19
paulotroguilho <paulo.troguilho@outlook.com.br>
2017-12-15 16:22:37 -0200

    css
    css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,15 @@
 import {NgModule} from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CommentModule } from './home/home/comment/comment.module'
+import { CommentComponent } from './home/home/comment/comment.component';
 const routes: Routes = [
     //{ path: '', redirectTo: 'contact', pathMatch: 'full'},
-    { path: ' ./home/', redirectTo: './home/home/comment/comment.module.ts#CommentModule'},
-    { path: 'home/home/comment', loadChildren: './home/home/comment/comment.module.ts#CommentModule' },
-    { path: 'card', loadChildren: './card-materias/card-materias.module.ts#CardModule' }
+    // { path: 'home/home/comment', redirectTo: 'home/home/comment/comment.module.ts#CommentModule', canActivate:[CommentComponent]},
+    // { path: 'home/home/comment', loadChildren: './home/home/comment/comment.module.ts#CommentModule' },
+    // { path: 'card', loadChildren: './card-materias/card-materias.module.ts#CardModule' }
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home/home/comment', component: CommentComponent },
+  
   ];
 
 @NgModule({
